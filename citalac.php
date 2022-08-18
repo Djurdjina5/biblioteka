d3df77c00a06d148e7c999342f072aee12c25894
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-18 18:47:30 +0200

    dodata sesija

--- citalac.php
+++ citalac.php
@@ -9,75 +9,67 @@ if (!isset($_SESSION['username'])) {
 $username = $_SESSION['username'];
 $books = Book::getByUser($username, $conn);
 if (!$books) {
-    echo "Nastala je greška pri preuzimanju podataka";
+    echo "Greška";
     die();
 }
-if ($books->num_rows == 0) {
-    echo "Nema prijava na kolokvijume";
-    die();
-}
+if (!$books) {
+    echo "Nemate zaduženih knjiga!";
+} else {
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <?php
-    include "head.php";
-    ?>
-</head>
+    <head>
+        <?php
+        include "head.php";
+        ?>
+    </head>
 
-<body>
-    <?php
-    include "header.php";
-    ?>
-
-
-
-    <section>
-        <div class="container h-custom mt-5 pt-5">
-            <?php
-            echo "Zdravo, " . $_SESSION['username'] . " " . $_SESSION['last_name'] . "!";
-            // if (!$books) {
-            //     echo "Nemate zaduženih knjiga!";
-            // } else {
-
-
-            ?>
-            <p>Spisak tvojih knjiga je dat u tabeli:</p>
-            <table class="table  table-warning table-striped mt-5 pt-5">
-                <thead>
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Naziv</th>
-                        <th scope="col">Autor</th>
-                        <th scope="col">Rok za vraćanje</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    while ($row = $books->fetch_assoc()) :
+    <body>
+        <?php
+        include "header.php";
+        ?>
+        <section>
+            <div class="container h-custom mt-5 pt-5">
+                <h1><?php
+                    echo "Zdravo, " . $_SESSION['first_name'] . " " . $_SESSION['last_name'] . "!";
                     ?>
+                </h1>
+                <p>Spisak tvojih zaduženih knjiga je dat u tabeli:</p>
+                <table class="table  table-warning table-striped mt-5 pt-5">
+                    <thead>
                         <tr>
-                            <td><?php echo $row['id'] ?></td>
-                            <td><?php echo $row['title'] ?></td>
-                            <td><?php echo $row['author'] ?></td>
-                            <td><?php echo $row['deadline'] ?></td>
+                            <th scope="col">Id</th>
+                            <th scope="col">Naziv</th>
+                            <th scope="col">Autor</th>
+                            <th scope="col">Rok za vraćanje</th>
                         </tr>
-                    <?php
-                    endwhile;
-                    // }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </section>
-
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($row = $books->fetch_assoc()) :
+                        ?>
+                            <tr>
+                                <td><?php echo $row['id'] ?></td>
+                                <td><?php echo $row['title'] ?></td>
+                                <td><?php echo $row['author'] ?></td>
+                                <td><?php echo $row['deadline'] ?></td>
+                            </tr>
+                        <?php
+                        endwhile;
+                        // }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
 
     <?php
-    include "footer.php";
+}
+include "footer.php";
     ?>
 
-</body>
+    </body>
 
-</html>
+    </html>
