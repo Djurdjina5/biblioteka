8fa8cf33320bc8920009db64b1331723b96e2eac
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-18 23:24:40 +0200

    dodat handler

---
+++ handler/add.php
@@ -0,0 +1,15 @@
+<?php
+require "../dbBroker.php";
+require "../model/books.php";
+session_start();
+error_log('pogodio me');
+if (!isset($_SESSION['username'])) {
+    header('Location: index.php');
+    exit();
+}
+?>
+<?php
+$book = new Book($_POST['id'], $_POST['title'], $_POST['author'], null, null);
+$odg = Book::add($book, $conn);
+return $odg;
+?>
