34d7f92155a772602f611e4e6e71d019c902a65e
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-18 19:29:44 +0200

    radi zaduzi

--- handler/update.php
+++ handler/update.php
@@ -1,11 +1,18 @@
 <?php
-include "dbBroker.php";
-include "./model/books.php";
+require "../dbBroker.php";
+require "../model/books.php";
 session_start();
 if (!isset($_SESSION['username'])) {
     header('Location: index.php');
     exit();
+} ?>
+<?php
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $username = $_SESSION['username'];
+    $deadline = date('Y-m-d', strtotime(date('y-m-d') . ' + 14 days'));
+    $odg = Book::zaduzi($id, $deadline, $username, $conn);
+} else {
+    echo "Ne radi";
 }
-
-
 ?>
