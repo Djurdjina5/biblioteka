20ca78898d9da525d478efa925c5000d268efbe9
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-18 23:24:57 +0200

    dodata funkcija dodaj

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ function zaduzi(){
         $.ajax({
             url: './handler/update.php',
             type: 'POST',
-            data: { id:id
-            },
+            data: {'id': id},
             success: function(data){
                 if (data) {
                 console.log("updated");
@@ -17,8 +16,39 @@ function zaduzi(){
                 }
             },
             error: function(jqXHR, textStatus, errorThrown){
-                $('#error').html("oops" + errorThrown);
+                $('#error').html("Greška" + errorThrown);
             }
         });
     
 }
+function dodaj(){
+  var id = document.forms["addForm"]["id"].value;
+  var title = document.forms["addForm"]["title"].value;
+  var author = document.forms["addForm"]["author"].value;
+  console.log(id);
+  console.log(title);
+  console.log(author);
+
+  if (id == "" || title == "" || author == "") {
+    alert("Forms must be filled out");
+    return false;
+  }
+
+    $.ajax({
+            url: './handler/add.php',
+            method:'POST',
+            type: 'POST',
+            data: {'id': id, 'title':title, 'author':author},
+            dataType: 'json',
+            success: function(data){
+                if (data) {
+                console.log("added");
+                } else {
+                    $('#error').load("custom/static/error.html");
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                $('#error').html("Greška" + errorThrown);
+            }
+        });
+  }
