ede8949db283d02d065c2662566b81101ec8cf54
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-19 22:22:25 +0200

    dodat razduzi handler

---
+++ handler/razduzi.php
@@ -0,0 +1,16 @@
+<?php
+require "../dbBroker.php";
+require "../model/books.php";
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: index.php');
+    exit();
+} ?>
+<?php
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $odg = Book::razduzi($id, $conn);
+} else {
+    echo "Ne radi";
+}
+?>
