59dc31479807dab06c4b6a451ada5be9d80bd782
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-19 22:23:38 +0200

    dodato delete dugme koje ne radi

--- bibliotekar.php
+++ bibliotekar.php
@@ -7,6 +7,7 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 $username = $_SESSION['username'];
+$books = Book::getAll($conn);
 ?>
 
 <!DOCTYPE html>
@@ -46,6 +47,45 @@ $username = $_SESSION['username'];
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row d-flex justify-content-left text-center mt-5 pt-5">
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <h1>Spisak knjiga</h1>
+                <table class="table  table-warning table-striped mt-5 pt-5">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Naziv</th>
+                            <th scope="col">Autor</th>
+                            <th scope="col">Person</th>
+                            <th scope="col">Rok za vraćanje</th>
+                            <th scope="col">Izbriši</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($row = $books->fetch_assoc()) :
+                        ?>
+                            <tr>
+                                <td id="red"><?php echo $row['id'] ?></td>
+                                <td><?php echo $row['title'] ?></td>
+                                <td><?php echo $row['author'] ?></td>
+                                <td><?php echo $row['person'] ?></td>
+                                <td><?php echo $row['deadline'] ?></td>
+                                <td>
+                                    <button id="btnIzbrisi" type="button" class="btn btn-danger btn-block" onclick="izbrisi()">Izbrisi</button>
+                                </td>
+                            </tr>
+                        <?php
+                        endwhile;
+                        // }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 
 
 
