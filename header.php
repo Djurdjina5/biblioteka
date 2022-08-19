5e633216e4386759467206605888dedaf4f176a5
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-19 22:23:09 +0200

    dodat deo za logout i izbrisan deo za index

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+require "dbBroker.php";
+require "model/user.php";
+?>
 <!DOCTYPE html>
 <nav class="navbar navbar-expand navbar-light bg-warning fixed-top">
     <a class="navbar-brand" href="#">
@@ -8,8 +12,19 @@
                 <a class="nav-link" href="index.php">Početna</a>
             </li>
             <li class="nav-item">
+                <a class="nav-link" href="bibliotekar.php">Bibliotekar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="citalac.php">Čitalac</a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="knjige.php">Knjige</a>
             </li>
         </ul>
+    </div>
+    <div class="nav-item ml-0">
+        <a class="nav-link" href="logout.php">
+            <h5>Log out</h5>
+        </a>
     </div>
 </nav>
