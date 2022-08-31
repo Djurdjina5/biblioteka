54cd37f9fb3ee73383133ce332767fb225ddef19
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-31 13:04:25 +0200

    dodati uslovi za navbar

--- header.php
+++ header.php
@@ -1,30 +1,50 @@
 <?php
 require "dbBroker.php";
 require "model/user.php";
+$username = $_SESSION['username'];
+$type = $_SESSION['type'];
 ?>
+
 <!DOCTYPE html>
 <nav class="navbar navbar-expand navbar-light bg-warning fixed-top">
     <a class="navbar-brand" href="#">
         <img src="./img/books.png" width="30" height="30" alt="">Biblioteka</a>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Početna</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="bibliotekar.php">Bibliotekar</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="citalac.php">Čitalac</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="knjige.php">Knjige</a>
-            </li>
+
+            <?php if (!isset($username)) { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"> Početna</a>
+                </li>
+
+            <?php } ?>
+
+            <?php if ($type == "bibliotekar") { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="bibliotekar.php">Bibliotekar</a>
+                </li>
+            <?php } ?>
+
+            <?php if ($type == "citalac") { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="citalac.php">Čitalac</a>
+                </li>
+            <?php } ?>
+            <?php if ($type == "citalac") { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="knjige.php">Knjige</a>
+                </li>
+            <?php } ?>
+
+
         </ul>
     </div>
-    <div class="nav-item ml-0">
-        <a class="nav-link" href="logout.php">
-            <h5>Log out</h5>
-        </a>
-    </div>
+    <?php if (isset($username)) { ?>
+        <div class="nav-item ml-0">
+            <a class="nav-link" href="logout.php">
+                <h5>Log out</h5>
+            </a>
+        </div>
+    <?php } ?>
+
 </nav>
